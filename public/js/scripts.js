ac6ecafb4cbcf887d2bb9e9b7cee6829b2954ae0
ariel <ariel29111978@hotmail.com>
2021-07-21 21:34:08 -0300

    Se agregó el botón del menú y su funcionalidad

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,1 @@
-"use strict";console.log("hola");
+"use strict";console.log("hola");var btnMenu=document.getElementById("btn-menu");btnMenu.addEventListener("click",(function(){btnMenu.firstChild.classList.toggle("btn-menu__bar--active"),btnMenu.childNodes[2].firstElementChild.classList.toggle("header__menu--active")}));
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,7 @@
 console.log("hola")
+const btnMenu=document.getElementById('btn-menu');
+
+btnMenu.addEventListener('click', () =>{    
+    btnMenu.firstChild.classList.toggle('btn-menu__bar--active')
+    btnMenu.childNodes[2].firstElementChild.classList.toggle('header__menu--active')
+})
