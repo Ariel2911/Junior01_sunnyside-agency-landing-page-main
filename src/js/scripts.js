adce8bb5c576cad3a87fe67af57ade0973dbf4b5
ariel <ariel29111978@hotmail.com>
2021-08-02 16:30:14 -0300

    Se colocaron 'ids' para la navegación interna y se modificó el JavasScript

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,13 @@
 const btnMenu=document.getElementById('btn-menu');
 const menu= document.querySelector('.menu');
 
-//Despliega el menú
+//Activa el menú
 btnMenu.addEventListener('click', () =>{    
     btnMenu.firstChild.classList.toggle('btn-menu__bar--active')
     menu.classList.toggle('menu--active')
+})
+//cuando un elemento del menú es seleccionado, desactiva el menú 
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('menu--active')
+    btnMenu.firstChild.classList.remove('btn-menu__bar--active')
 })
